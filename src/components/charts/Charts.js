588b9a5ad34a8bf00db327edb6c967554267a81e
Alperen Kapusuz <63202119+alperenkapusuz@users.noreply.github.com>
2021-09-02 13:23:07 +0300

    charts.css tamamlandı

--- src/components/charts/Charts.js
+++ src/components/charts/Charts.js
@@ -6,6 +6,7 @@ import supercup from "../../api/supercup.json";
 import superligcupImg from "../../images/superligcup.png";
 import supercupImg from "../../images/supercup.png";
 import ziraatcupImg from "../../images/ziraatcup.png";
+import "./Charts.css"
 
 import {
   BarChart,
@@ -23,62 +24,63 @@ const Charts = () => {
 
   return (
     <Row>
-      <Card>
+      <Card className="card">
         <Col>
           <h1>Süper Lig Kupası<img src={superligcupImg} alt="cup" width="60" /></h1>
           <BarChart
             width={1250}
             height={500}
             data={data}
-            margin={{ top: 30, right: 30, left: 20, bottom: 5 }}
+            margin={{ top: 30, right: 30, left: 20, bottom: 30 }}
           >
             <CartesianGrid strokeDasharray="3 3" />
             <XAxis dataKey="teamName" tick={false} />
             <YAxis />
-            <Bar dataKey="championships" fill="#8884d8">
-              <LabelList dataKey="teamName" position="insideTop" angle="35" />
+            <Bar dataKey="championships" fill="#ffd700">
+              <LabelList dataKey="teamName" position="insideTop" angle="40" fontSize={15}/>
             </Bar>
           </BarChart>
         </Col>
       </Card>
 
-      <Card>
+      <Card className="card">
         <Col>
         <h1>Ziraat Türkiye Kupası<img src={ziraatcupImg} alt="cup" width="60" /></h1>
           <BarChart
             width={1250}
             height={500}
             data={data2}
-            margin={{ top: 30, right: 30, left: 20, bottom: 5 }}
+            margin={{ top: 30, right: 30, left: 20, bottom: 30 }}
           >
             <CartesianGrid strokeDasharray="3 3" />
             <XAxis dataKey="teamName" tick={false} />
             <YAxis />
-            <Bar dataKey="championships" fill="#8884d8">
-              <LabelList dataKey="teamName" position="insideTop" angle="35" />
+            <Bar dataKey="championships" fill="#ff4040">
+              <LabelList dataKey="teamName" position="insideTop" angle="35" fontSize={17}/>
             </Bar>
           </BarChart>
         </Col>
       </Card>
 
-      <Card>
+      <Card className="card">
         <Col>
         <h1>Süper Kupa<img src={supercupImg} alt="cup" width="60" /></h1>
           <BarChart
             width={1250}
             height={500}
             data={data3}
-            margin={{ top: 30, right: 30, left: 20, bottom: 5 }}
+            margin={{ top: 30, right: 30, left: 20, bottom: 30 }}
           >
             <CartesianGrid strokeDasharray="3 3" />
             <XAxis dataKey="teamName" tick={false} />
             <YAxis />
             <Bar dataKey="championships" fill="#8884d8">
-              <LabelList dataKey="teamName" position="insideTop" angle="35" />
+              <LabelList dataKey="teamName" position="top" angle="0" fontSize={17}/>
             </Bar>
           </BarChart>
         </Col>
       </Card>
+
     </Row>
   );
 };
